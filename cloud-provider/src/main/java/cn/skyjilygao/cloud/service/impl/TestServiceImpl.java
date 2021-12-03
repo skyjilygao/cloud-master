7bf346775bf8c47b5ab6ae65e7a3fd314657c637
Administrator <gaoqihua@empowerwin.com>
2021-12-03 13:44:02 +0800

    优化

--- cloud-provider/src/main/java/cn/skyjilygao/cloud/service/impl/TestServiceImpl.java
+++ cloud-provider/src/main/java/cn/skyjilygao/cloud/service/impl/TestServiceImpl.java
@@ -1,16 +1,20 @@
 package cn.skyjilygao.cloud.service.impl;
 
 import cn.skyjilygao.cloud.TestService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.stereotype.Component;
 
 import java.time.ZonedDateTime;
 
+@Slf4j
 @Component
 @DubboService(group="DEFAULT_GROUP")
 public class TestServiceImpl implements TestService {
     @Override
     public String sayHello(String name) {
-        return String.format("Hello %s, time:%s", name, ZonedDateTime.now());
+        String format = String.format("Hello %s, time:%s", name, ZonedDateTime.now());
+        log.info(format);
+        return format;
     }
 }
